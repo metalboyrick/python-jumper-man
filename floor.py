1d215ed7e13d7c4b98821aeddfc3d2b4745d8adc
metalboyrick <53423618+metalboyrick@users.noreply.github.com>
2020-09-16 15:51:16 +0700

    Added assets + moving background feature

---
+++ floor.py
@@ -0,0 +1,26 @@
+import pygame
+
+# draw floors easily
+class Floor():
+    # constructor for floor class
+    def __init__(self, image_path, width, height):
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.width = width
+        self.height = height
+        self.x_pos = 0
+        self.y_pos = 0
+
+    # scale twice
+    def scale_twice(self):
+        self.image = pygame.transform.scale2x(self.image)
+        self.width = 2 * self.width
+        self.height = 2 * self.height
+
+
+    # this will draw a scrolling floor
+    def draw_to_screen(self, screen_obj,vertical_pos, screen_width, speed):
+        if self.x_pos < screen_width * -1:
+            self.x_pos = 0
+        self.x_pos -= speed
+        screen_obj.blit(self.image,(self.x_pos, vertical_pos))
+        screen_obj.blit(self.image,(screen_width + self.x_pos, vertical_pos))
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame, sys
+from floor import Floor
 
 # initialisation
 pygame.init()
@@ -6,14 +7,22 @@ clock = pygame.time.Clock()
 
 # Screen initialisation
 screen_width = 700
-screen_height = 200
+screen_height = 300
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Jumper Man")
+
+# import assets
+# background
+bg_surface = pygame.image.load('assets/bg.png').convert()   # converting to PyGame-friendly
+
+# floor
+floor = Floor(image_path='assets/tile.png',width=497,height=99)
+floor.scale_twice()
 
 # Game loop
 while True:
 
-    # Handle input
+    # this is the event loop
     for event in pygame.event.get():
 
         # Handle quit event
@@ -21,8 +30,20 @@ while True:
             pygame.quit()
             sys.exit()
 
+    
+
+    # draws background on surface
+    screen.blit(bg_surface,(0,0))
+
+    # floor movement
+    floor.draw_to_screen(screen_obj=screen,\
+                        vertical_pos=200,\
+                        screen_width=screen_width,\
+                        speed=6)
+
+
     # window update
-    pygame.display.flip()
+    pygame.display.update()
     clock.tick(60)
 
 
